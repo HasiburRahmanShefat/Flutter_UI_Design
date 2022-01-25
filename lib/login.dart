d9df90aa401cd3fbe7c20ed65482a49c8d65b437
HasiburRahmanShefat <cfusou551@gmail.com>
2022-01-25 10:57:12 +0600

    first commit for this project

---
+++ lib/login.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:ui_design/register.dart';
+
+class MyLogin extends StatefulWidget {
+  const MyLogin({Key? key}) : super(key: key);
+
+  @override
+  _MyLoginState createState() => _MyLoginState();
+}
+
+class _MyLoginState extends State<MyLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/log.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 55, top: 160,),
+              child:  Text(
+
+                'Welcome',
+
+                style: TextStyle(color: Colors.lightBlue, fontSize: 33),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.5,
+                    right: 35,
+                    left: 35),
+
+                //EdgeInsets.only
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                          fillColor: Colors.teal.shade800,
+                          hintText: "Email",
+                          hintStyle:
+                              TextStyle(fontSize: 18.0, color: Colors.white),
+                          prefixIcon: const Icon(
+                            Icons.email_outlined,
+                            color: Colors.blue,
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40))),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          fillColor: Colors.teal.shade800,
+                          hintText: "Password",
+                          hintStyle:
+                              TextStyle(fontSize: 18.0, color: Colors.white),
+                          prefixIcon: const Icon(
+                            Icons.lock_outline_rounded,
+                            color: Colors.blue,
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(40))),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Sign In',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700),
+                        ),
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.yellow,
+                          child: IconButton(
+                            color: Colors.red,
+                            onPressed: () {},
+                            icon: Icon(Icons.arrow_forward),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) =>  MyRegister()),
+                              );
+                            },
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
+                            )),
+                        TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Forget Password',
+                              style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontSize: 20,
+                                color: Colors.blue,
+                              ),
+                            )),
+
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ui_design/login.dart';
+
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyLogin(),
+  ));
+}
